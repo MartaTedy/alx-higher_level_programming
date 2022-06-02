93e959e9f526fc6a26337fa70bdd9ebb224cff49
MartaTedy <tewodrosmartha@gmail.com>
2022-06-02 21:58:39 +0300

    first commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if length < 2:
         print("0 arguments.")
     elif length == 2:
-        print("1 arguments:")
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print("{} {}:".format(length - 1, "arguments"))
