04df28b7267eca579768c26d3745b2a61b1b201c
MartaTedy <tewodrosmartha@gmail.com>
2022-06-02 20:57:25 +0300

    first commit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-    a, b = 10, 5
+    a = 10
+    b = 5
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} + {} = {}".format(a, b, sub(a, b)))
